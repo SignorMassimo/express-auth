d9facb81160d0e833b6eb6c66cfc9f5ed267b38e
Signor_P <124287199+SignorMassimo@users.noreply.github.com>
2026-04-06 10:09:21 +0400

    Release 0.0.2 sync

---
+++ routes/auth.route.js
@@ -0,0 +1,30 @@
+import { Router } from 'express'
+import { UserModel } from '../database/models/user.model'
+
+const router = Router()
+
+router.get('/all', (req, res) => {
+    res.json(UserModel.findAll())
+})
+
+router.post('/login', (req, res) => {
+    const { name } = req.body,
+        user = UserModel.find('name', name)
+    if (user == null) {
+        res.send('user not found')
+        return
+    }
+    res.send('success login')
+})
+
+router.post('/register', (req, res) => {
+    const { name } = req.body,
+        user = UserModel.find('name', name)
+    if (user != null) {
+        res.send('user aleardy exists')
+        return
+    }
+    res.send('success register')
+})
+
+export default router
